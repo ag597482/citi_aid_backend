48f2e3cfddbf7f5c213524e731c0f55da167480a
Aman Gupta <amangupta@Amans-MacBook-Air.local>
2025-11-07 20:56:07 +0530

    function to add 2 numbers added

--- src/main/java/com/project/citi_aid_backend/controller/HelloController.java
+++ src/main/java/com/project/citi_aid_backend/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.project.citi_aid_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String welcome() {
-        return "Hello world";
+    public int welcome(@RequestParam int a, @RequestParam int b) {
+        return a+b;
     }
 }
